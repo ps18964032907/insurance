85ad0bd9b7af357620590c20f6843f160e070fa8
Administrator <2286010407@qq,com>
2020-08-13 12:33:26 +0800

    admin 模板更新

--- policy-admin/policy-admin-api/src/main/java/com/insurance/policy/admin/feign/PolicyAdminFeign.java
+++ policy-admin/policy-admin-api/src/main/java/com/insurance/policy/admin/feign/PolicyAdminFeign.java
@@ -1,8 +1,10 @@
 package com.insurance.policy.admin.feign;
 
+import com.insurance.policy.admin.domain.CommercialPolicy;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @Author 潘升
@@ -11,5 +13,44 @@ import org.springframework.web.bind.annotation.PathVariable;
  */
 @FeignClient("policy-admin-service")
 public interface PolicyAdminFeign {
+
+    /**
+     * @return com.insurance.policy.admin.domain.CommercialPolicy 交强险和商业险组合对象
+     * @Author 潘升
+     * @Description //TODO 添加保单接口
+     * @Date 12:19 2020/8/13
+     * @Param [commercialPolicy] 交强险和商业险组合对象
+     * @since 1.0.0
+     */
+    @RequestMapping("savePolicy")
+    CommercialPolicy savePolicy(CommercialPolicy commercialPolicy);
+
+    /**
+     * @return void
+     * @Author 潘升
+     * @Description //TODO 核实单接口
+     * @Date 12:25 2020/8/13
+     * @Param [commercialPolicy]
+     * @since 1.0.0
+     */
+    @RequestMapping("checkPolicy")
+    void checkPolicy(CommercialPolicy commercialPolicy);
+    
+    /**
+     * @Author 潘升 
+     * @Description //TODO 缴费接口
+     * @Date 12:32 2020/8/13
+     * @Param [] 
+     * @since 1.0.0
+     * @return void
+    */
+    @RequestMapping("payPolicy")
+    void payPolicy();
+
+/*
+    @RequestMapping("producePolicy")
+    void producePolicy();
+*/
+
 
 }
