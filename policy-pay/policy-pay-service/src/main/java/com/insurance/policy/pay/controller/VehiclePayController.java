6a8c8764c818e9b4a1cab770d8be85f401f61594
lisi <111111>
2020-08-15 11:16:56 +0800

    支付

--- policy-pay/policy-pay-service/src/main/java/com/insurance/policy/pay/controller/VehiclePayController.java
+++ policy-pay/policy-pay-service/src/main/java/com/insurance/policy/pay/controller/VehiclePayController.java
@@ -32,6 +32,7 @@ public class VehiclePayController {
 
     @Autowired
     RestTemplate restTemplate;
+
     @Autowired
     VehiclePolicyService vehiclePolicyService;
 
@@ -42,16 +43,14 @@ public class VehiclePayController {
     }
 
     @RequestMapping("/payMoney/{id}")
-    public void payMoney(HttpServletResponse httpResponse,@PathVariable("id") long id) throws IOException {
+    public void payMoney(HttpServletResponse httpResponse, @PathVariable("id") long id) throws IOException {
         /*  String object = restTemplate.getForObject("http://policy-pay-service/pay/payMoney", String.class);*/
         AlipayClient alipayClient = new DefaultAlipayClient(alipayConfig.gatewayUrl, alipayConfig.app_id, alipayConfig.merchant_private_key,
                 "json", alipayConfig.charset, alipayConfig.alipay_public_key, alipayConfig.sign_type);
-
         //设置请求参数
         AlipayTradePagePayRequest alipayRequest = new AlipayTradePagePayRequest();
         alipayRequest.setReturnUrl(alipayConfig.return_url);
         alipayRequest.setNotifyUrl(alipayConfig.notify_url);
-
 //        //商品价格总额
 //        String orderNo = (String) session.getAttribute("orderNo");
       /*  if (id ) {
@@ -61,45 +60,39 @@ public class VehiclePayController {
         if (vehiclePolicyService == null) {
             return;
         }
-
         //商品名称
-
         String subject = new String("保单缴费");
-
         //商品描述，可以为空
         String body = "蜗牛保险";
-
         //填充业务参数
         AlipayTradeAppPayModel model = new AlipayTradeAppPayModel();
         model.setBody(body);
-       // model.setOutTradeNo(policyNo);
+        //model.setOutTradeNo(policyNo);
         model.setTotalAmount(String.valueOf(vehiclePolicyMain.getDuePremium()));
         model.setSubject(subject);
         model.setProductCode("FAST_INSTANT_TRADE_PAY");
-
         alipayRequest.setBizModel(model);
         alipayRequest.setReturnUrl(alipayConfig.return_url);
         alipayRequest.setNotifyUrl(alipayConfig.notify_url);
-
         String form = "";
         try {
             form = alipayClient.pageExecute(alipayRequest).getBody(); //调用SDK生成表单
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         httpResponse.setContentType("text/html;charset=utf-8");
         httpResponse.getWriter().write(form);//直接将完整的表单html输出到页面
         httpResponse.getWriter().flush();
         httpResponse.getWriter().close();
     }
+
     @RequestMapping("/notify/{id}")
-    public void doNotify(@PathVariable("id") long id){
+    public void doNotify(@PathVariable("id") long id) {
         int result = vehiclePolicyService.updateVehiclePolicyStatues(id);
     }
 
     @RequestMapping("/return/{id}")
-    public String doReturn(@PathVariable("id") long id){
+    public String doReturn(@PathVariable("id") long id) {
         int result = vehiclePolicyService.updateVehiclePolicyStatues(id);
         return "business/HTML/index.html";
     }
