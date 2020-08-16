58e31c75fcff55639fe8d92f7d6d0868a2f79143
sjh <742673570@qq.com>
2020-08-16 14:35:07 +0800

    sjh_message_center 2020-8-16 14:34

--- policy-message-center/policy-message-center-api/src/main/java/com/insurance/policy/message/fegin/PolicyMessageCenterFegin.java
+++ policy-message-center/policy-message-center-api/src/main/java/com/insurance/policy/message/fegin/PolicyMessageCenterFegin.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.math.BigDecimal;
+
 /**
  * @Author sjh
  * @creat 2020/8/14 10:19
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient(value = "policy-message-center-service",path = "/MessageCenter")
 public interface PolicyMessageCenterFegin {
     @RequestMapping("/SendToFinance")
-    long sendFinanceMessage(long id,String email);
+    long sendFinanceMessage(BigDecimal duePremium, long id, String email);
 
     @RequestMapping("/SendToPolicyMain")
     long sendPolicyMainMessage(long id,String email);
